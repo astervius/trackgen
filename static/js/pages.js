8840c3e7ae7a4a08137fac191fec20d28df00639
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2025-03-20 20:31:01 +0000

    Refactor button click handling

--- static/js/pages.js
+++ static/js/pages.js
@@ -1,14 +1,18 @@
-document.querySelectorAll("#input-type button").forEach(button => {
-	button.addEventListener("click", () => {
-		let index = 0;
-		let elem = button;
-		while (elem !== null) {
-			elem = elem.previousElementSibling;
-			index++;
-		}
-		document.querySelectorAll(".page")[0].style.marginLeft = -95 * (index - 1) + "vw";
+const inputTypeButtons = document.querySelectorAll("#input-type button");
+const firstPage = document.querySelector(".page");
+const PAGE_WIDTH_VW = 95;
 
-		document.querySelectorAll("#input-type button").forEach(button => button.className = "");
-		button.className = "selected";
+const buttonsArray = Array.from(inputTypeButtons);
+
+inputTypeButtons.forEach(button => {
+	button.addEventListener("click", () => {
+		const index = buttonsArray.indexOf(button);
+		
+		// update page position
+		firstPage.style.marginLeft = -PAGE_WIDTH_VW * index + "vw";
+
+		// update button styles
+		inputTypeButtons.forEach(btn => btn.classList.remove("selected"));
+		button.classList.add("selected");
 	});
 });
